f5c3f9a23db1be79591c9f60af62b30d465a849b
Jan Werkhoven <jan.werkhoven@gmail.com>
2016-06-18 14:08:05 +1000

    Set up Grunt framework

---
+++ Gruntfile.js
@@ -0,0 +1,202 @@
+module.exports = function(grunt) {
+
+  'use strict';
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: 'dist',
+          hostname: 'localhost',
+          livereload: true,
+          open: false
+        }
+      }
+    },
+
+    watch: {
+      handlebars: {
+        files: ['src/templates/**/*.hbs', 'src/templates/**/*.json', 'src/templates/layout.html '],
+        tasks: ['handlebarslayouts', 'sitemap']
+      },
+      sass: {
+        files: ['src/styles/**/*.scss'],
+        tasks: ['sass', 'postcss']
+      },
+      js: {
+        files: ['src/js/**/*.js'],
+        tasks: ['jshint', 'uglify', 'concat', 'clean:temp']
+      },
+      assets: {
+        files: ['src/public/**/*'],
+        tasks: ['copy']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js'],
+        tasks: ['build']
+      },
+      options: {
+        livereload: true,
+      }
+    },
+
+    handlebarslayouts: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/templates/',
+          src: ['**/*.hbs', '!partials/*'],
+          dest: 'dist/',
+          ext: '.html',
+        }],
+        options: {
+          partials: ['src/templates/partials/*.hbs', 'src/templates/layout.html'],
+          basePath: 'src/templates/',
+          modules: ['src/templates/helpers/helpers-*.js']
+        }
+      }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          sourcemap: 'none',
+          noCache: true
+        },
+        files: {
+          'dist/assets/css/main.min.css': ['src/styles/main.scss']
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require('pixrem')(),
+          require('autoprefixer')({
+            browsers: ['> 1% in AU']
+          }),
+          require('cssnano')()
+        ]
+      },
+      dist: {
+        src: 'dist/assets/css/main.min.css'
+      }
+    },
+
+    jshint: {
+      files: ['src/js/main.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'temp/main.min.js': ['src/js/main.js']
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';\n\n',
+      },
+      dist: {
+        files: {
+          'dist/assets/js/main.min.js': ['src/js/vendor/jquery.min.js', 'src/js/vendor/velocity.min.js', 'temp/main.min.js']
+        },
+      },
+    },
+
+    clean: {
+      dist: {
+        src: ['dist/']
+      },
+      temp: {
+        src: ['temp/']
+      }
+    },
+
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'src/public/',
+          src: ['**'],
+          dest: 'dist/'
+        }]
+      }
+    },
+
+    // BUG: Adds /dist to every URL...
+    xml_sitemap: {
+      custom_options: {
+        options: {
+          siteRoot: 'http://finsecptx.com/',
+          changefreq: 'monthly',
+          priority: '0.5',
+          dest: 'dist/'
+        },
+        files: [{
+          expand: true,
+          cwd: 'dist/',
+          src: ['**/*.html', '!**/google*.html'],
+        }]
+      }
+    },
+
+    // Temporary until sitemap bug is fixed
+    replace: {
+      sitemap_dist: {
+        src: 'dist/sitemap.xml',
+        dest: 'dist/sitemap.xml',
+        replacements: [{
+          from: '/dist/',
+          to: '/'
+        }, {
+          from: '<feed>',
+          to: ''
+        }, {
+          from: '</feed>',
+          to: ''
+        }]
+      }
+    }
+
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-handlebars-layouts");
+  grunt.loadNpmTasks('grunt-html-prettyprinter');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-xml-sitemap');
+  grunt.loadNpmTasks('grunt-text-replace');
+
+  // Available commands
+  grunt.registerTask('default', ['build', 'serve']);
+  grunt.registerTask('build', ['clean:dist', 'copy', 'handlebarslayouts', 'sass', 'postcss', 'jshint', 'uglify', 'concat', 'sitemap', 'clean:temp']);
+  grunt.registerTask('sitemap', ['xml_sitemap', 'replace:sitemap_dist']);
+  grunt.registerTask('serve', ['connect', 'watch']);
+
+};
--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# grunt-finsec-ptx
-Website of UK pension to Australian super transfer consultant
-### A pigeoning big change
+# Finsec PTX website
+
+#### What
+The [website](http://finsecptx.com/) of an Adelaide based consultants specialised in transfering UK Pension to Australian supers.
+
+#### Highlights
+* Built with Grunt, Handlebars, SASS, Atom & Git
+* 100% mobile responsive and retina-ready
+* Featuring an elaborate "rolling one field form"
+* Coded by Jan Werkhoven (Yoda) and Richard Verheyen (Luke)
+* Designed by [Hannah Sutton](http://hannahsuttondesign.com/)
+* Hosted on SSD Ubuntu Nginx servers
+* Planning to launch Juli 2016
+
+#### Setup
+1. Install the [Grunt](http://gruntjs.com/getting-started), [NPM](https://nodejs.org/en/download/) and [Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git) commands globally
+1. Create a directory and move to it: `mkdir -p ~/janwerkhoven/finsecptx && cd $_`
+2. Clone this repo: `git clone git@github.com:janwerkhoven/grunt-finsec-ptx.git .`
+3. Install packages: `npm install`
+4. Run Grunt (build dist, start server, start watchers): `grunt`
+5. Go to [http://localhost:9000/](http://localhost:9000/)
+6. Open code with whatever fancies your panties, I use [Atom](https://atom.io/): `atom .`
+7. Happy coding!
+
+#### Contact
+Get in touch on [LinkedIn](https://au.linkedin.com/pub/jan-werkhoven/10/64/b30), [GitHub](https://github.com/janwerkhoven) or <a href="mailto:jw@nabu.io" target="_blank">jw@nabu.io</a>.
+
+--------------
+
+**Jan Werkhoven**  
+Web Developer  
+& UI Designer
