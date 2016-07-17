e3662c0b6729c5c273fa9703e338b0b20a88cd5f
Jan Werkhoven <jan.werkhoven@gmail.com>
2016-07-17 18:06:10 +1000

    Update meta titles

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,8 +58,8 @@ module.exports = function(grunt) {
           basePath: 'src/templates/',
           modules: ['src/templates/helpers/helpers-*.js'],
           context: {
-            formLink: '/eligibility-test-UK-pension-Australian-super-transfer',
-            formTitle: 'Test whether you are eligibile for transfering your UK Pension to an Australian super'
+            formLink: '/Am-I-eligible-to-transfer-my-UK-pension-to-Australian-super',
+            formTitle: 'Am I eligible to transfer my UK Pension to Australian Super'
           }
         }
       }
