74d86ab566b62c963cbe74442b7f8786662d581e
Jan Werkhoven <jan.werkhoven@gmail.com>
2016-07-17 14:52:48 +1000

    Fix up CSS compilation

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  // Animation logic for diagram on About page
   var $process = $('#about #process');
   $process.find('h3, .img, .desc').on('click mouseenter', function() {
     $(this).parent('li').addClass('active').siblings('li').removeClass('active');
@@ -7,6 +8,7 @@ $(document).ready(function() {
 
   // Make any hashtag link scroll with animation to element with matching ID
   // Example: <a href="#features"> will scroll to element with ID #features
+  // Commonly found in the #hero of each page
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
